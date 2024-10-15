a9e3076b812804992c40b1a36c6b53479c2fb77c
mrico98 <manuelrico98@outlook.es>
2024-10-14 22:28:53 -0600

    (feat):(adding first spec example)

---
+++ TeamSpace.Middleware/TeamSpace.Domain/Specifications/Base/Specification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSpace.Domain.Specifications.Base;
+public class Specification<T> : ISpecification<T>
+{
+    public Specification(Expression<Func<T, bool>> criteria)
+    {
+        Criteria = criteria;
+    }
+
+    public Expression<Func<T, bool>> Criteria { get; }
+}
