b343ade8d727caca26d62289e9620d7ab46e66ef
mrico98 <manuelrico98@outlook.es>
2024-10-20 23:29:35 -0600

    (fix):(modifing SpaceByOwner spec

--- TeamSpace.Middleware/TeamSpace.Domain/Specifications/Space/SpaceByOwnerId.cs
+++ TeamSpace.Middleware/TeamSpace.Domain/Specifications/Space/SpaceByOwnerId.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TeamSpace.Domain.Entities.Base;
-using TeamSpace.Domain.Specifications.Base;
+﻿using TeamSpace.Domain.Specifications.Base;
 
 namespace TeamSpace.Domain.Specifications.Space;
-public class SpaceByOwnerId<Space> : Specification<Space> where Space : BaseEntity
+public class SpaceByOwnerId<T> : Specification<T> where T : Entities.Space
 {
     public SpaceByOwnerId(Guid ownerId) : base(e => e.Owner == ownerId)
     {
